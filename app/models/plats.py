152d39887a7fe07f6d6c7aa3d37b0f17476cab10
dholy <holyfrancisdushime@gmail.com>
2023-06-11 17:18:03 -0700

    update class Plat

--- app/models/plats.py
+++ app/models/plats.py
@@ -1,7 +1,7 @@
 from django.db import models
 from app.models import Menu
 
-class Plat(models):
+class Plat(models.Model):
     name=models.CharField( max_length=50, null=True)
     menu=models.ForeignKey(Menu, on_delete=models.CASCADE)
     description=models.TextField(max_length=50, null=True)
